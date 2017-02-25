42b30e75cebb6e167c91e53fffd67db00ada5614
Neil Aylor <neilaylor@hotmail.com>
2017-02-25 15:28:46 -0600

    Added Pre-Existing Code From my Computer

---
+++ Comic Sans/scrub.js
@@ -0,0 +1,69 @@
+
+
+
+//Changes p
+body = document.getElementsByTagName("body");
+console.log(body);
+body[0].style.fontFamily = "Comic Sans MS";
+//end change of p
+
+h1 = document.getElementsByTagName("h1");
+console.log(h1);
+for (var i = 0; i < h1.length; i ++){
+    h1[i].style.fontFamily = "Comic Sans MS";
+}
+
+
+a = document.getElementsByTagName("a");
+console.log(a);
+for (var i = 0; i < a.length; i ++){
+    a[i].style.fontFamily = "Comic Sans MS";
+}
+
+span = document.getElementsByTagName("span");
+console.log(span);
+for (var i = 0; i < span.length; i ++){
+    span[i].style.fontFamily = "Comic Sans MS";
+}
+
+cite = document.getElementsByTagName("cite");
+console.log(cite);
+for (var i = 0; i < cite.length; i ++){
+    cite[i].style.fontFamily = "Comic Sans MS";
+}
+
+input = document.getElementsByTagName("input");
+console.log(input);
+for (var i = 0; i < input.length; i ++){
+    input[i].style.fontFamily = "Comic Sans MS";
+}
+
+input = document.getElementsByTagName("h2");
+console.log(input);
+for (var i = 0; i < input.length; i ++){
+    input[i].style.fontFamily = "Comic Sans MS";
+}
+
+input = document.getElementsByTagName("h3");
+console.log(input);
+for (var i = 0; i < input.length; i ++){
+    input[i].style.fontFamily = "Comic Sans MS";
+}
+
+input = document.getElementsByTagName("h4");
+console.log(input);
+for (var i = 0; i < input.length; i ++){
+    input[i].style.fontFamily = "Comic Sans MS";
+}
+
+input = document.getElementsByTagName("h5");
+console.log(input);
+for (var i = 0; i < input.length; i ++){
+    input[i].style.fontFamily = "Comic Sans MS";
+}
+
+input = document.getElementsByTagName("h6");
+console.log(input);
+for (var i = 0; i < input.length; i ++){
+    input[i].style.fontFamily = "Comic Sans MS";
+}
